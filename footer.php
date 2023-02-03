0bb1aa759d9d85384dc0cec5ede37373c6428276
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2023-02-03 18:57:01 +0530

    added tailwind css

---
+++ footer.php
@@ -0,0 +1,18 @@
+<div class="mt-10">
+<div class="flex flex-wrap -mb-4 text-center text-white">
+  <div class="w-1/3 mb-4 bg-gray-400 h-12 ">
+    Made with ❤️
+  </div>
+  <div class="w-1/3 mb-4 bg-gray-500 h-12">
+    About Us
+  </div>
+  <div class="w-1/3 mb-4 bg-gray-400 h-12">
+    Career Page
+  </div>
+  
+</div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
